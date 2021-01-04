837f4299fa1e26aba95d2702e723064f835fc29a
Dmitry <49843752+BullitStudio@users.noreply.github.com>
2021-01-04 12:56:59 +0200

    Init project

---
+++ src/components/RoomsFilter.tsx
@@ -0,0 +1,86 @@
+import React, { useContext } from 'react';
+import { RoomContext } from '../store/context';
+import Title from './Title';
+import Loading from './Loading';
+
+interface IPropsRoomsFilter {
+    rooms: any[];
+}
+
+// Get all unique items
+const getUnique = (items: any[], value: any) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+// Another method of using context without HOC or provider
+const RoomsFilter = ({rooms}: IPropsRoomsFilter) => {
+    const roomContext = useContext(RoomContext);
+    if (roomContext) {
+    const { handleChange, handleChecked, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets } = roomContext;
+    let types = getUnique(rooms, 'type');
+    types = ['Все', ...types];
+
+    let people = getUnique(rooms, 'capacity');
+
+    return (
+        <section className="filter-container">
+            <Title title="Найти номер" />
+            <form className="filter-form">
+                {/* select type */}
+                <div className="form-group">
+                    <label htmlFor="type">Тип</label>
+                    <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>
+                        {types.map((item: any, index: number) => {
+                            return <option key={index} value={item}>{item}</option>
+                        })}
+                    </select>
+                </div>
+                {/* end of select type */}
+                {/* guest */}
+                <div className="form-group">
+                    <label htmlFor="capacity">Гостей</label>
+                    <select name="capacity" id="capacity" value={capacity} className="form-control" onChange={handleChange}>
+                        {people.map((item: any, index: number) => {
+                            return <option key={index} value={item}>{item}</option>
+                        })}
+                    </select>
+                </div>
+                {/* end of guest */}
+                {/* rooms price */}
+                <div className="form-group">
+                    <label htmlFor="price">
+                       Цена ${price}
+                    </label>
+                    <input type="range" id="price" name="price" min={minPrice} max={maxPrice} value={price} onChange={handleChange} className="form-control"/>
+                </div>
+                {/* end of rooms price */}
+                {/* size */}
+                <div className="form-group">
+                    <label htmlFor="size">Размеры номера</label>
+                    <div className="size-inputs">
+                        <input type="number" name="minSize" min={0} id="size" value={minSize} onChange={handleChange} className="size-input"/>
+                        <input type="number" name="maxSize" min={0} id="size" value={maxSize} onChange={handleChange} className="size-input"/>
+                    </div>
+                </div>
+                {/* end of size */}
+                {/* extras */}
+                <div className="form-group">
+                    <div className="single-extra">
+                        <input type="checkbox" name="breakfast" id="breakfast" checked={breakfast} onChange={handleChecked}/>
+                        <label htmlFor="breakfast">Завтрак</label>
+                    </div>
+                     <div className="single-extra">
+                        <input type="checkbox" name="pets" id="pets" checked={pets} onChange={handleChecked}/>
+                        <label htmlFor="pets">Питомцы</label>
+                    </div>
+                </div>
+                {/* end of extras */}
+            </form>
+        </section>
+    )
+    } else {
+        return <Loading />
+    }
+}
+
+export default RoomsFilter;
